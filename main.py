92ee72f4a49515ccc0ca7f49d6524ac99f4c728e
faisalsadi <fesalsadi@gmail.com>
2023-05-31 20:27:01 +0300

    final product

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from mapsCalculation import calculate_depth_disparity
 def reproject_to_3d(image, K, depth_map):
     # Get image dimensions
     height, width = image.shape[:2]
@@ -48,88 +49,48 @@ def project_to_camera_plane(points_3d, camera_matrix):
     return points_2d
 
 
-# def calculate_baseline(left_disparity_map, right_disparity_map, left_intrinsics):
-#     # Get the focal length from the intrinsics matrix
-#     focal_length = left_intrinsics[0, 0]
-#
-#     # Calculate the baseline
-#     baseline = focal_length * np.abs(left_intrinsics[0, 2] - right_intrinsics[0, 2]) / np.mean(
-#         left_disparity_map - right_disparity_map)
-#
-#     return baseline
+
 
 
 ## Main
 ############################################################################################
-# Load the image, camera matrix, and depth map
-focals = [0,688.000061035156,1120,800,896,689.029357910156]
-for j in range(1,6):
-    image1 = cv2.imread(f"Data/set_{j}/im_left.jpg")
-    K = np.array([[576, 0, 511.5], [0, 576, 217.5], [0, 0, 1]], dtype=np.float32)
-    depth_map = np.loadtxt('Data/example/depth_left.txt',delimiter=',')
-    depth_matrix = np.loadtxt(f"Data/set_{j}/depth_left.txt",delimiter=',')
-    K1 = np.array([[focals[j], 0, 511.5], [0, focals[j], 217.5], [0, 0, 1]], dtype=np.float32)
-
-    # points_3d = reproject_to_3d(image, K, tt)
-    # for i in range (11):
-    #     # Call the function to reproject image coordinates to 3D space
-    #     ext=np.array([[1, 0, 0,-0.01*i], [0, 1, 0,0], [0, 0, 1,0]], dtype=np.float32)
-    #     camera_matrix =np.dot(K ,ext )
-    #
-    #     # Call the function to project 3D points to the camera plane
-    #     points_2d = project_to_camera_plane(points_3d, camera_matrix)
-    #
-    #     # Create a blank image with the same size as the original image
-    #     reprojected_image = np.zeros_like(image)
-    #
-    #     # Copy pixel values from the original image to the reprojected image
-    #     for y in range(image.shape[0]):
-    #         for x in range(image.shape[1]):
-    #             if not(np.isnan(points_2d[y, x]).any()):
-    #                 # Get the projected 2D coordinates of the point
-    #                 point_2d = points_2d[y, x]
-    #
-    #                 # Round the 2D coordinates to the nearest pixel
-    #                 point_2d_rounded = np.round(point_2d).astype(int)
-    #                 if point_2d_rounded[1]< image.shape[0] and point_2d_rounded[1] >=0 and point_2d_rounded[0]< image.shape[1] and point_2d_rounded[0] >= 0:
-    #                     # Copy the RGB pixel value from the original image to the reprojected image
-    #                     reprojected_image[point_2d_rounded[1], point_2d_rounded[0]] = image[y, x]
-    #
-    #     # Display the reprojected image
-    #     plt.imshow(reprojected_image)
-    #     plt.show()
-    #     cv2.imwrite(f"s{i+1}.jpg", reprojected_image)
-    #     print(i+1,"/",11)
-
-
-    points_3d = reproject_to_3d(image1, K1, depth_matrix)
-    for i in range (11):
+if __name__ == '__main__':
+    # calculate disparity's and depth's maps and images and store them in set{i} directory
+    calculate_depth_disparity()
+    focals = [0,688.000061035156,1120,800,896,689.029357910156]
+    # this loop generates the 11 required synth images
+    for j in range(1,6):
+        image = cv2.imread(f"Data/set_{j}/im_left.jpg")
+        depth_map = np.loadtxt(f"Data/set_{j}/depth_left.txt",delimiter=',')
+        K = np.array([[focals[j], 0, 511.5], [0, focals[j], 217.5], [0, 0, 1]], dtype=np.float32)
         # Call the function to reproject image coordinates to 3D space
-        ext=np.array([[1, 0, 0,-0.01*i], [0, 1, 0,0], [0, 0, 1,0]], dtype=np.float32)
-        camera_matrix =np.dot(K1 ,ext )
+        points_3d = reproject_to_3d(image, K, depth_map)
+        for i in range (11):
+            ext=np.array([[1, 0, 0,-0.01*i], [0, 1, 0,0], [0, 0, 1,0]], dtype=np.float32)
+            camera_matrix =np.dot(K ,ext )
 
-        # Call the function to project 3D points to the camera plane
-        points_2d = project_to_camera_plane(points_3d, camera_matrix)
+            # Call the function to project 3D points to the camera plane
+            points_2d = project_to_camera_plane(points_3d, camera_matrix)
 
-        # Create a blank image with the same size as the original image
-        reprojected_image = np.zeros_like(image1)
+            # Create a blank image with the same size as the original image
+            reprojected_image = np.zeros_like(image)
 
-        # Copy pixel values from the original image to the reprojected image
-        for y in range(image1.shape[0]):
-            for x in range(image1.shape[1]):
-                if not(np.isnan(points_2d[y, x]).any()):
-                    # Get the projected 2D coordinates of the point
-                    point_2d = points_2d[y, x]
+            # Copy pixel values from the original image to the reprojected image
+            for y in range(image.shape[0]):
+                for x in range(image.shape[1]):
+                    if not(np.isnan(points_2d[y, x]).any()):
+                        # Get the projected 2D coordinates of the point
+                        point_2d = points_2d[y, x]
 
-                    # Round the 2D coordinates to the nearest pixel
-                    point_2d_rounded = np.round(point_2d).astype(int)
-                    if point_2d_rounded[1]< image1.shape[0] and point_2d_rounded[1] >=0 and point_2d_rounded[0]< image1.shape[1] and point_2d_rounded[0] >= 0:
-                        # Copy the RGB pixel value from the original image to the reprojected image
-                        reprojected_image[point_2d_rounded[1], point_2d_rounded[0]] = image1[y, x]
+                        # Round the 2D coordinates to the nearest pixel
+                        point_2d_rounded = np.round(point_2d).astype(int)
+                        if point_2d_rounded[1]< image.shape[0] and point_2d_rounded[1] >=0 and point_2d_rounded[0]< image.shape[1] and point_2d_rounded[0] >= 0:
+                            # Copy the RGB pixel value from the original image to the reprojected image
+                            reprojected_image[point_2d_rounded[1], point_2d_rounded[0]] = image[y, x]
 
-        # Display the reprojected image
-        plt.imshow(reprojected_image)
-        plt.show()
-        cv2.imwrite(f"Data/set_{j}/synth_{i+1}.jpg", reprojected_image)
-        print(i+1,"/",11)
+            # Display the reprojected image
+            plt.imshow(reprojected_image)
+            plt.show()
+            cv2.imwrite(f"Data/set_{j}/synth_{i+1}.jpg", reprojected_image)
+            print(i+1,"/",11)
 
